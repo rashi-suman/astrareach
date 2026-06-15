eb32f142f40d2b3954ccd478909ce9b4da98610b
rashisuman76-ops <rashisuman76@gmail.com>
2026-06-15 15:00:06 +0530

    feat(utils): add slug generator

---
+++ utils/slug.js
@@ -0,0 +1,9 @@
+module.exports = function slug(str) {
+  return String(str)
+    .toLowerCase()
+    .normalize('NFKD')
+    .replace(/[̀-ͯ]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+    .slice(0, 80);
+};
